754f43703e37580a91f52be6232cdb3f566731a7
Neloy Saha <neloy.saha456@gmail.com>
2024-02-10 14:33:56 +0600

    added vercel speed analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import reduxStore from "./reduxStore.js";
 
@@ -15,5 +16,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </BrowserRouter>
     </Provider>
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
